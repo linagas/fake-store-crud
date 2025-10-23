ad774522f51929a5f0d9a540e1269689db1aebeb
Leila Inagas <tu_correo@ejemplo.com>
2025-10-23 07:34:01 -0300

    chore: setup Angular 14 project with Material, routing and base toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,26 @@
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { ProductsService } from './core/services/products.service';
+import { LS_PRODUCTS_WORKING } from './core/constants/storage-keys';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+import { ProductsModule } from './features/products.module';
+
+export function appInitFactory(products: ProductsService) {
+  return async () => {
+    products.resetWorking();  
+    await products.init();
+  };
+}
 
 @NgModule({
   declarations: [
@@ -10,9 +28,22 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    ProductsModule,
+    MatSnackBarModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: appInitFactory,
+      deps: [ProductsService],
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
